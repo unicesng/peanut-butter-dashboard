71260ce05980030390192fb6588eb44440bbcb0c
unicesng <135223038+unicesng@users.noreply.github.com>
2025-04-07 03:17:32 +0800

    integrate report function

--- frontend/src/api/energyApis.tsx
+++ frontend/src/api/energyApis.tsx
@@ -1,4 +1,15 @@
 import axios from 'axios';
+import { S3Client, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { report } from 'process';
+
+const s3Client = new S3Client({
+  region: "us-east-1", 
+  credentials: {
+    accessKeyId: `${process.env.NEXT_PUBLIC_ACCESS_KEY}`,
+    secretAccessKey: `${process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY}`
+    }
+});
 
 export const GetAverageDailyConsumption = async () => {
     try {
@@ -64,4 +75,32 @@ export const GetProjectedEnergyConsumption = async () => {
         console.error('Error fetching projected energy consumption data:', error);
         throw error;
     }
+}
+
+export const GetReport = async () => {
+    try {
+        console.log('Access Key:', process.env.NEXT_PUBLIC_ACCESS_KEY);
+        console.log('Secret Key:', process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY);
+
+        const url = `https://tindpxvddg.execute-api.us-east-1.amazonaws.com/prod/trigger-report-generation`;
+        const response = await axios.post(url);
+        console.log('Response:', response);
+        const body = JSON.parse(response.data.body);
+        const reportId = body['report_id']
+        
+        const s3ObjectKey = `reports/${reportId}.csv`;
+    
+        // Step 3: Generate a presigned URL for the file from S3
+        const command = new GetObjectCommand({
+          Bucket: "peanut-butter-project",
+          Key: s3ObjectKey,
+        });
+    
+        const signedUrl = await getSignedUrl(s3Client, command, { expiresIn: 86400 });
+    
+        return signedUrl;
+      } catch (error) {
+        console.error('Error fetching report data or generating presigned URL:', error);
+        throw error;
+      }
 }
